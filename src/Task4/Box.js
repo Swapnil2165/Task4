fd9c02214bf49ed71e7a05fa8f89262b3db03c48
swapnil2165 <swapnilchikanepatil2165@gmail.com>
2023-08-08 01:39:32 +0530

    message

--- src/Task4/Box.js
+++ src/Task4/Box.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
-import { TextField, Button, Typography, Box } from '@mui/material';
-import './Box.css';
 
 function App() {
   const [box1Value, setBox1Value] = useState('');
@@ -41,44 +39,57 @@ function App() {
 
   const handleChartButtonClick = () => {
     const newData = [
-      { title: 'Box 1', value: parseInt(box1Value), color: '#E38627' },
-      { title: 'Box 2', value: parseInt(box2Value), color: '#C13C37' },
+      { title: 'Box 1', value: parseInt(box1Value), color: '#F46300' },
+      { title: 'Box 2', value: parseInt(box2Value), color: '#44DE28' },
     ];
     setChartData(newData);
     setShowChart(true);
   };
 
   return (
-    <div className="container">
-      <Box className="box">
-        <Typography variant="h5" component="div" className="title" gutterBottom>
-          Box 1:
-        </Typography>
-        <TextField
-          type="number"
-          value={box1Value}
-          onChange={handleBox1Change}
-          error={validationMessage1 !== ''}
-          helperText={validationMessage1}
-          className="text-field"
-        />
-        <Typography variant="h5" component="div" className="title" gutterBottom>
-          Box 2:
-        </Typography>
-        <TextField
-          type="number"
-          value={box2Value}
-          onChange={handleBox2Change}
-          error={validationMessage2 !== ''}
-          helperText={validationMessage2}
-          className="text-field"
-        /><br /><br />
-        <Button variant="contained" onClick={handleChartButtonClick} className="button">
-          Create Chart
-        </Button><br /><br />
-        
-        {showChart && <PieChart data={chartData} className="chart" />}
-      </Box>
+    <div className="container mt-5">
+       <div className='row'>
+          <div className='card center'>
+             <div className='card-body center'>
+                <div class="row">
+                     <div class="form-group col-sm-6">
+                          <label for="inputBox">Box 1</label>
+                          <input
+                             id="inputBox"
+                             type="number"
+                             value={box1Value}
+                             onChange={handleBox1Change}
+                             error={validationMessage1 !== ''}
+                             helperText={validationMessage1}
+                             className="text-field form-control"
+                              />
+                       </div>
+                        <div class="form-group col-sm-6">
+                            <label for="inputBox2">Box 2</label>
+                            <input
+                             id="inputBox2" 
+                             type="number"
+                             value={box2Value}
+                             onChange={handleBox2Change}
+                             error={validationMessage2 !== ''}
+                             helperText={validationMessage2}
+                            className="text-field form-control"
+                               />
+                       </div>
+                       <div className='form-group col-sm-6'>
+                         <button variant="primary"  onClick={handleChartButtonClick} className="btn-primary">
+                          Create Chart
+                          </button>
+                      </div>
+                </div>
+                <div className='row'>
+                  <div className='col-sm-4 ml-3'>
+                    {showChart && <PieChart data={chartData} className="chart" />}
+                  </div>
+                </div>
+            </div>
+         </div>
+      </div>
     </div>
   );
 }
